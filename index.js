8b3358608fdb39137b690149f3c7b7ae2f03ab61
L-U-P-I-N <skjccm@gmail.com>
2025-07-02 06:05:08 +0300

    secand commit

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 //path
-
 //root
 app.get('/',(req,res)=>{
     res.sendFile("./home/views/home.html",{root:__dirname})
 })
-
-
 //port
 const port = 3000;
+//connect db
+mongoose.connect('mongodb+srv://skjccm:Pa$$w0rd@nodejfirst.2hkfz4v.mongodb.net/all-data?retryWrites=true&w=majority&appName=nodejFirst').then(()=>{
 app.listen(port, () => {
   console.log(`http://localhost:${port}/`);
 });
-//address
+}).catch((e)=>{
+console.log(e) 
+});
